860207207597a957432db801cebf56eb4681eafb
sandiiux <sandygz1010@gmail.com>
2021-08-04 12:06:36 -0500

    01, sandraGonzalez, se finalizó con la implementación de los formularios VerPedidos, VerProductos y de Cerrar.

---
+++ WebProducto/VerProductos.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassCapaLogicaNegocios;
+using ClassCapaEntidades;
+
+namespace WebProducto
+{
+    public partial class VerProductos : System.Web.UI.Page
+    {
+        ClassLogicaCarniceria objLog = null;
+        string receptor = "";
+        string[] atrap = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            objLog = new ClassLogicaCarniceria();
+            receptor = Request.QueryString["query"].ToString();
+            atrap = (string[])Session["Datos"];
+            if (atrap != null)
+            {
+                lbAux.InnerHtml = atrap[1];
+                string m = "", i = "", t = "";
+                GridView1.DataSource = objLog.PedidosGrid3(int.Parse(receptor), ref m, ref i, ref t);
+                GridView1.DataBind();
+            }
+            else
+            {
+                Session["Datos"] = null;
+                Response.Redirect("Cerrar.aspx");
+            }
+        }
+    }
+}
